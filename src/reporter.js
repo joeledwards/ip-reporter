202e3a8c01aceb67ce22d3470f39de5002b876b1
Joel Edwards <joeledwards@gmail.com>
2017-03-29 13:00:46 +0000

    Updated reporter to run directly or as module.

--- src/reporter.js
+++ src/reporter.js
@@ -18,7 +18,10 @@ function getPublicIp() {
 
 // Fetch all local interface info.
 function getInterfaces() {
-  return P.resolve(os.networkInterfaces());
+  return P.resolve({
+    hostname: os.hostname(),
+    interfaces: os.networkInterfaces(),
+  });
 }
 
 // Run the network check.
@@ -26,15 +29,17 @@ function checkNetwork() {
   getPublicIp()
 
   .then(ip => {
-    return getInterfaces().then(interfaces => {
+    return getInterfaces().then(({hostname, interfaces}) => {
       return {
-        interfaces: interfaces,
+        hostname,
+        interfaces,
         public_ip: ip,
       };
     });
   })
 
   .then(info => {
+    console.log(`IP Info:\n${JSON.stringify(info, null, 2)}`);
     // TODO:
     // - check with database
     // - report to IP service
@@ -47,9 +52,12 @@ function checkNetwork() {
   .finally(() => setTimeout(checkNetwork, 60000));
 }
 
-module.exports = {
-  checkNetwork,
-  getPublicIp,
-  getInterfaces,
-};
-
+if (require.main === module) {
+  checkNetwork();
+} else {
+  module.exports = {
+    checkNetwork,
+    getPublicIp,
+    getInterfaces,
+  };
+}
